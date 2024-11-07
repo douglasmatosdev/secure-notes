80b1d8fba0ec749644f525b39a626b19324ef235
douglasmatosdev <douglasmatosdev@gmail.com>
2024-11-07 16:45:30 -0300

    Deleting uncompressed files

--- class_materials_and_resources/18_section/sec-536-email-func/main/java/com/secure/notes/repositories/PasswordResetTokenRepository.java
+++
@@ -1,9 +0,0 @@
-package com.secure.notes.repositories;
-
-import com.secure.notes.models.PasswordResetToken;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PasswordResetTokenRepository extends JpaRepository<PasswordResetToken, Long> {
-}
