22d223deaceba7774c6153d4d629054e9dcbf50f
douglasmatosdev <douglasmatosdev@gmail.com>
2024-11-07 13:55:04 -0300

    remove uncompressed files

--- class_materials_and_resources/16_section/sec-523-service-layer/main/java/com/secure/notes/repositories/AuditLogRepository.java
+++
@@ -1,7 +0,0 @@
-package com.secure.notes.repositories;
-
-import com.secure.notes.models.AuditLog;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AuditLogRepository extends JpaRepository<AuditLog, Long> {
-}
